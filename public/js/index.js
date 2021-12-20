cba47e75659281c98afbf52c2b0b6d4c77215e94
Julien Boucly <julien.boucly@apini.fr>
2021-12-20 23:24:10 +0100

    Edit template of learn

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,17 @@
-// Your code here
+/**
+ *  @description Return list for random users
+ */
+async function getRandomUsers() {
+    try {
+        const res = await axios.get("https://random-data-api.com/api/users/random_user?size=100");
+        return res.data;
+    } catch (err) {
+        console.error(`[INDEX JS ERROR] : ${err}`);
+    }
+}
+
+/**
+ * Your code here ⤵️
+ */
+
+getRandomUsers().then(users => console.log(users));
