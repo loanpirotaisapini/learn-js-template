daf0558d70906cb5f588e60a384d6263f00184f2
loanpirotaisapini <91887520+loanpirotaisapini@users.noreply.github.com>
2022-01-10 14:50:56 +0100

    feat(users): Setting users informations in table (#2)

--- public/js/index.js
+++ public/js/index.js
@@ -11,7 +11,36 @@ async function getRandomUsers() {
 }
 
 /**
- * Your code here ⤵️
+ * @description Set the users informations in the table
  */
+async function setUsersInTable() {
+    var users = await getRandomUsers();
 
-getRandomUsers().then(users => console.log(users));
+    var usersTable = document.getElementById('users-table');
+    for (var i = 0; i < users.length; i++) {
+        var tr = document.createElement('tr');
+        usersTable.appendChild(tr);
+
+        var userAvatar = users[i].avatar;
+        var img = document.createElement('img');
+        img.src = userAvatar;
+
+        var tdAvatar = document.createElement('td');
+        tdAvatar.appendChild(img);
+        tr.appendChild(tdAvatar);
+
+        var tdName = document.createElement('td');
+        tdName.innerHTML = users[i].last_name;
+        tr.appendChild(tdName);
+        
+        var tdFirstname = document.createElement('td');
+        tdFirstname.innerHTML = users[i].first_name;
+        tr.appendChild(tdFirstname);
+        
+        var tdEmail = document.createElement('td');
+        tdEmail.innerHTML = users[i].email;
+        tr.appendChild(tdEmail);
+    }
+}
+
+setUsersInTable().then(console.log("Data loaded and filled successfully"));
