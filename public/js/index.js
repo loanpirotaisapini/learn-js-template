8176a0d0de13e902de865df0266804607d98b884
Loan Pirotais <loan.pirotais@apini.fr>
2022-01-13 14:23:51 +0100

    feat(update): Possible to update the informations of a user

--- public/js/index.js
+++ public/js/index.js
@@ -27,8 +27,8 @@ class UsersTable {
             res.data.forEach(element => {
                 //Changing avatar set and size
                 let avatarParameters = element.avatar.split('?', 1);
-                let newParameters = '?size=100x100&set=set4';
-                let userAvatar = avatarParameters.concat(newParameters);
+                avatarParameters.push('?size=100x100&set=set4');
+                let userAvatar = avatarParameters.join();
 
                 let user = new User(
                     element.uid,
@@ -100,21 +100,103 @@ class UsersTable {
 
             tr.appendChild(tdEmail);
 
+            // ---------------------- Update button ------------------------
+            let updateButton = document.createElement('button');
+            updateButton.innerHTML = "Modifier";
+            updateButton.classList.add('update-button');
+            updateButton.addEventListener('click', (event) => {
+                event.preventDefault();
+                this.openModal(tr.id);
+            });
+
             // ---------------------- Delete button ------------------------
             let deleteButton = document.createElement('button');
             deleteButton.innerHTML = "Supprimer";
             deleteButton.classList.add('delete-button');
-            deleteButton.addEventListener('click', () => {
+            deleteButton.addEventListener('click', (event) => {
+                event.preventDefault();
                 this.deleteUser(tr.id);
             });
 
+            // --------------------- Setting actions -----------------------
             let tdActions = document.createElement('td');
+            tdActions.appendChild(updateButton);
             tdActions.appendChild(deleteButton);
 
             tr.appendChild(tdActions);
 
             tbody.appendChild(tr);
         });
+    }
+
+    /**
+     * @description Open the update user modal 
+     */
+    openModal(uid) {
+        let modal = document.getElementById('user-modal-form');
+        let user = this.users.find(user => user.uid === uid);
+
+        // ------------------ Setting avatar value in input -----------------
+        let inputAvatar = document.getElementById('input-user-avatar');
+        inputAvatar.value = user.avatar;
+
+        // ----------------- Setting lastname value in input ----------------
+        let inputLastname = document.getElementById('input-user-lastname');
+        inputLastname.value = user.lastname;
+
+        // ---------------- Setting firstname value in input ----------------
+        let inputFirstname = document.getElementById('input-user-firstname');
+        inputFirstname.value = user.firstname;
+
+        // ------------------- Setting email value in input -----------------
+        let inputEmail = document.getElementById('input-user-email');
+        inputEmail.value = user.email;
+
+        // Opening modal
+        modal.classList.add('open');
+
+        let validateButton = document.getElementById('validate-button');
+        validateButton.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const form = document.getElementById('user-form');
+            var formData = new FormData(form);
+
+            let updatedInformations = {}
+
+            for (let key of formData.keys()) {
+                updatedInformations[key] = formData.get(key);
+            };
+
+            this.updateUser(uid, updatedInformations);
+            uid = '';
+            modal.classList.remove('open');
+
+            this.loadUsers();
+        });
+
+        let cancelButton = document.getElementById('close-button');
+        cancelButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            uid = '';
+            modal.classList.remove('open');
+        });
+    }
+
+    /**
+     * @description Update a user's informations
+     */
+    updateUser(uid, updatedInformations) {
+        let updatedUser = new User(
+            uid,
+            updatedInformations.inputAvatar,
+            updatedInformations.inputLastname,
+            updatedInformations.inputFirstname,
+            updatedInformations.inputEmail
+        );
+
+        let userToUpdateIndex = this.users.findIndex(user => user.uid === uid);
+        this.users.splice(userToUpdateIndex, 1, updatedUser);
     }
 
     /**
